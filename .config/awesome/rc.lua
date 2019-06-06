72beb9a7b5dc9a9e04918f4c0bee47df67731e05
aswinmohanme <dev.aswinmohan@gmail.com>
2019-06-06 23:46:53 +0530

    Set default shortcute with consistency

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -383,7 +383,7 @@ globalkeys = my_table.join(
               {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
-    awful.key({ modkey, "Shift"   }, "q", awesome.quit,
+    awful.key({ modkey, "Control"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
 
     awful.key({ altkey, "Shift"   }, "l",     function () awful.tag.incmwfact( 0.05)          end,
