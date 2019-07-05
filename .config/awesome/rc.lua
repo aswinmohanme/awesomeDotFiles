045e5166b6de8488e54896958ef8846b0b299f39
aswinmohanme <dev.aswinmohan@gmail.com>
2019-07-05 20:48:05 +0530

    Add gnoem screenshot and fix rofi update

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -270,7 +270,7 @@ root.buttons(my_table.join(
 globalkeys = my_table.join(
     -- Take a screenshot
     -- https://github.com/lcpz/dots/blob/master/bin/screenshot
-    awful.key({ altkey }, "p", function() os.execute("screenshot") end,
+    awful.key({ altkey }, "p", function() os.execute("gnome-screenshot") end,
               {description = "take a screenshot", group = "hotkeys"}),
 
     -- X screen locker
